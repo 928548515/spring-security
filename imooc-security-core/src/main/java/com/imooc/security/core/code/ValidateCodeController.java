5770e9a049f63fbfff04902ab63931207895fbb0
pengyu <pengyu120919@163.com>
2019-04-29 18:10:15 +0800

    2019-04-18

--- imooc-security-core/src/main/java/com/imooc/security/core/code/ValidateCodeController.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/code/ValidateCodeController.java
@@ -2,6 +2,7 @@ package com.imooc.security.core.code;
 
 import org.springframework.social.connect.web.HttpSessionSessionStrategy;
 import org.springframework.social.connect.web.SessionStrategy;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -30,139 +31,59 @@ public class ValidateCodeController {
 
     @GetMapping("/code/image")
     public void createCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         ImageCode imageCode = createImageCode(request);
         sessionStrategy.setAttribute(new ServletWebRequest(request), SESSION_KEY, imageCode);
         ImageIO.write(imageCode.getImage(), "JEPG", response.getOutputStream());
-
     }
 
-    class VerifyUtil{
-        /** 验证码字符集 */
-        private  final char[] chars = {
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        /**
-         * 字符数量
-         */
-        private static final int SIZE = 4;
-
-        /**
-         * 干扰线数量
-         */
-        private static final int LINES = 5;
-        /**
-         * 宽度
-         */
-        private static final int WIDTH = 80;
-        /**
-         * 高度
-         */
-        private static final int HEIGHT = 40;
-        /**
-         * 字体大小
-         */
-        private static final int FONT_SIZE = 30;
-
-        /**
-         * 生成随机验证码及图片
-         * Object[0]：验证码字符串；
-         * Object[1]：验证码图片。
-         */
-        public  Object[] createImage() {
-            StringBuffer sb = new StringBuffer();
-            // 1.创建空白图片
-            BufferedImage image = new BufferedImage(
-                    WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-            // 2.获取图片画笔
-            Graphics graphic = image.getGraphics();
-            // 3.设置画笔颜色
-            graphic.setColor(Color.LIGHT_GRAY);
-            // 4.绘制矩形背景
-            graphic.fillRect(0, 0, WIDTH, HEIGHT);
-            // 5.画随机字符
-            Random ran = new Random();
-            for (int i = 0; i < SIZE; i++) {
-                // 取随机字符索引
-                int n = ran.nextInt(chars.length);
-                // 设置随机颜色
-                graphic.setColor(getRandomColor());
-                // 设置字体大小
-                graphic.setFont(new Font(
-                        null, Font.BOLD + Font.ITALIC, FONT_SIZE));
-                // 画字符
-                graphic.drawString(
-                        chars[n] + "", i * WIDTH / SIZE, HEIGHT * 2 / 3);
-                // 记录字符
-                sb.append(chars[n]);
-            }
-            // 6.画干扰线
-            for (int i = 0; i < LINES; i++) {
-                // 设置随机颜色
-                graphic.setColor(getRandomColor());
-                // 随机画线
-                graphic.drawLine(ran.nextInt(WIDTH), ran.nextInt(HEIGHT),
-                        ran.nextInt(WIDTH), ran.nextInt(HEIGHT));
-            }
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try {
-                ImageIO.write(image, "jpeg", baos);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return new Object[]{sb.toString(), baos.toByteArray()};
-
-        }
-
-        /**
-         * 随机取色
-         */
-        public  Color getRandomColor() {
-            Random ran = new Random();
-            Color color = new Color(ran.nextInt(256),
-                    ran.nextInt(256), ran.nextInt(256));
-            return color;
-        }
-    }
-
-    public ImageCode createImageCode(HttpServletRequest request) {
+    private ImageCode createImageCode(HttpServletRequest request){
         int width = 67;
         int height = 23;
-
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics g = image.getGraphics();
-        //随机操作对象
         Random random = new Random();
-        g.fillRect(0,0,width,height);
+        g.setColor(getRandColor(200, 250));
+        g.fillRect(0, 0, width, height);
+        g.setFont(new Font("Times New Roman", Font.ITALIC, 20));
+        g.setColor(getRandColor(160, 200));
+        for (int i = 0; i < 155; i++) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            int xl = random.nextInt(12);
+            int yl = random.nextInt(12);
+            g.drawLine(x, y, x + xl, y + yl);
+        }
 
-        return new ImageCode();
+        String sRand = "";
+        for (int i = 0; i < 4; i++) {
+            String rand = String.valueOf(random.nextInt(10));
+            sRand += rand;
+            g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
+            g.drawString(rand, 13 * i + 6, 16);
+        }
 
+        g.dispose();
+        return new ImageCode(image, sRand, 60);
     }
-
 
     /**
-     * 生成随机验证码
-     * @param type  类型
-     * @param length   长度
-     * @param exChars  排除的字符
+     * 生成随机背景条纹
+     *
+     * @param fc
+     * @param bc
      * @return
      */
-    public static String getRandomCode(int type,int length,String exChars) {
+    private Color getRandColor(int fc, int bc) {
         Random random = new Random();
-        int i = 0;
-        StringBuffer sb = new StringBuffer();
-            while (i < length) {
-                int t = random.nextInt(123);
-                if ((t >= 97 || (t >= 65 && t <= 90) || (t >= 48 && t <= 57)) && (exChars == null || exChars.indexOf((char) t) < 0)) {
-                    sb.append((char) t);
-                    i++;
-                }
-            }
-            return sb.toString();
+        if (fc > 255) {
+            fc = 255;
+        }
+        if (bc > 255) {
+            bc = 255;
+        }
+        int r = fc + random.nextInt(bc - fc);
+        int g = fc + random.nextInt(bc - fc);
+        int b = fc + random.nextInt(bc - fc);
+        return new Color(r, g, b);
     }
-
-
 }
